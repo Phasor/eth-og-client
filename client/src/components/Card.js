36ac6bde7f0f85694f5baf5791255e5023823294
ben <phas0ruk@gmail.com>
2021-11-22 02:38:58 -0800

    changes color palette css

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 
 
 const Card = styled(BootstrapCard)`
-  background-color: rgba(42, 181, 170);
+  background-color: #A8DADC;
   width: 100%;
   box-shadow: rgb(0 0 0 / 1%) 0px 0px 1px, rgb(0 0 0 / 4%) 0px 4px 8px, rgb(0 0 0 / 4%) 0px 16px 24px,
     rgb(0 0 0 / 1%) 0px 24px 32px;
   border-radius: 15px;
-  border-color: #7DDE92; 
+  border-color: #141414; 
   border-width: 1.5px;
   padding: 20px;
 `;
