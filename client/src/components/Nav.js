34b8b9f9594b03b3fafe1953adc78477c34f38d3
ben <phas0ruk@gmail.com>
2021-12-14 16:25:50 -0800

    changes nav adds images

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import MMConnectButton from './MMConnectButton';
+import sunglass from '../static/sunglasses.png';
+import eyes from '../static/Eyes.png';
+import styled from 'styled-components';
+
+const Sunglasses = styled.img.attrs({
+    src: sunglass,
+})`
+  height: 125px;
+`;
+
+const EyesImg = styled.img.attrs({
+    src: eyes,
+})`
+  height: 80px;
+`;
 
 export default function Nav(props) {
 
     return (
         <div className="nav">
             <div className="nav-container">
+                <div className="nav-images">
+                    <Sunglasses />
+                    <EyesImg className="nav-images__eyes" />
+                </div>
                 <MMConnectButton />
             </div>
         </div>
